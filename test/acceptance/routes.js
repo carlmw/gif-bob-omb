37e0b6a7c9ae1c80e84ccc0e1605ebfd3f690491
Carl Whittaker <carl359@gmail.com>
2013-08-09 22:42:35 +0100

    Correct acceptance tests

--- test/acceptance/routes.js
+++ test/acceptance/routes.js
@@ -1,13 +1,19 @@
 var request = require('supertest'),
     mockery = require('mockery'),
-    redis = require("redis"),
-    client = redis.createClient(),
+    redis = require('redis'),
+    client,
     app;
 
 describe('acceptance', function () {
   before(function () {
     mockery.registerSubstitute('./config.json', './test/acceptance/config.json');
     app = require('../../app');
+    client = redis.createClient();
+    client.set('hireme.png', 'http://aws.com/hireme.png');
+  });
+
+  after(function (done) {
+    client.del('hireme.png', done);
   });
 
   describe('GET /', function(){
@@ -19,15 +25,11 @@ describe('acceptance', function () {
   });
 
   describe('GET /frame/:file', function () {
-    after(function (done) {
-      client.del(__dirname + '/fixtures/hireme.png', done);
-    });
-
     it('should serve a file', function (done) {
       request(app)
         .get('/frame/hireme.png')
-        .expect(200)
-        .expect('Content-Type', 'image/png', done);
+        .expect(301)
+        .expect('Location', 'http://aws.com/hireme.png', done);
     });
   });
 });
